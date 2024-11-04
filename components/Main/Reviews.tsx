c70072fca60d646cc11c8ed141921ad9fbea437b
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-11-04 23:03:07 +0500

    #updat desktop

--- components/Main/Reviews.tsx
+++ components/Main/Reviews.tsx
@@ -82,7 +82,7 @@ const Reviews: FC = () => {
 
 
     return (
-        <div className="mt-[80px] 2xl:mt-[200px] py-[40px] 2xl:py-[100px] px-[20px] 4xl:px-[240px] 2xl:px-[50px] bg-[#F7F8FA]">
+        <div className="mt-[80px] 2xl:mt-[200px] py-[40px] 2xl:py-[100px] px-[20px] 4xl:pl-[240px] 2xl:px-[50px] bg-[#F7F8FA]">
             <p className="text-[26px] font-jost leading-[32px] 2xl:text-[45px] 2xl:leading-[59px] mb-[20px] 2xl:mb-[30px]">
                 Отзывы
             </p>
