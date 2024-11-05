5f602331f0a329b39f0e6789f0f4db467a1a6718
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-11-05 13:51:22 +0500

    #form

--- components/Main/Form.tsx
+++ components/Main/Form.tsx
@@ -13,7 +13,7 @@ const Form: FC = () => {
     <div className='mt-[80px] 2xl:mt-[200px]'>
         <div className=' bg-[#222E51] py-[40px] px-[16px] 2xl:py-[100px] 2xl:px-[50px]  4xl:px-[240px] flex flex-col 2xl:flex-row'>
             {/* TEXT AND BUTTON */}
-            <div className='w-[60%]'>
+            <div className='2xl:w-[60%]'>
                     <p className='text-[26px] 2xl:text-[50px] 2xl:leading-[62px]  uppercase text-titleWhite font-jost leading-[32px]'>Станьте частью <br className='2xl:hidden'/> сообщества <br/> архитекторов <br className='2xl:hidden'/> будущего!</p>
                     <p className='mt-[10px] 2xl:mt-[20px] 2xl:w-[50%] text-[15px] leading-[18px] text-[#A0A0A0]'>Присоединяйтесь к нам, чтобы вместе создавать проекты, которые вдохновляют и меняют города к лучшему</p>
                     <div className='flex flex-row flex-nowrap justify-between mt-[25px] 2xl:w-[45%] 2xl:mt-[50px]'>
@@ -21,7 +21,7 @@ const Form: FC = () => {
                         <button className='borderedButtonWhite w-[43%] flex items-center justify-center'>Стать партнером</button>
                     </div>
             </div>
-            <div className='mt-[30px] flex items-center gap-[8px] justify-center w-[40%] 2xl:items-end 2xl:mt-[220px] 2xl:ml-[-80px]'>
+            <div className='mt-[30px] flex items-center gap-[8px] justify-center 2xl:w-[40%] 2xl:items-end 2xl:mt-[220px] 2xl:ml-[-80px]'>
                 <Image  src={A} width={73} height={73} alt='A' quality={100} className='opacity-[20%] w-[73px] h-[73px] 2xl:w-[163px] 2xl:h-[163px]'/>
                 <Image  src={D} width={73} height={73} alt='A' quality={100} className='opacity-[20%] w-[73px] h-[73px] 2xl:w-[163px] 2xl:h-[163px]'/>
                 <Image  src={A} width={73} height={73} alt='A' quality={100} className='opacity-[20%] w-[73px] h-[73px] 2xl:w-[163px] 2xl:h-[163px] ml-[-8px]'/>
