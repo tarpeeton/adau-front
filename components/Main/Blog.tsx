79721592e279e590d4e628a586000579c07e6294
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-11-05 12:23:26 +0500

    #blog

--- components/Main/Blog.tsx
+++ components/Main/Blog.tsx
@@ -13,6 +13,7 @@ import { GrTechnology } from "react-icons/gr"
 import { GiTurban } from "react-icons/gi"
 import { FaDrawPolygon } from "react-icons/fa6"
 import { GrLinkNext } from "react-icons/gr"
+import { FaBorderAll } from "react-icons/fa6";
 // IMAGES
 import One from '@/public/blog/blogone.jpg'
 import Two from '@/public/blog/two.jpg'
@@ -24,7 +25,7 @@ const BlogData = [
     {
         url: One,
         date: '03.11.2024',
-        title: '10 ключевых архитектурных трендов: что будет популярно в ближайшие годы?',
+        title: '10 ключевых архитектурных трендов: что будет популярно в ближайшие годы?  что будет популярно в ближайшие годы?',
         category: "Архитектура"
     },
     {
@@ -40,19 +41,48 @@ const BlogData = [
         category: "Мировые тренды"
     },
     {
+        url: One,
+        date: '03.11.2024',
+        title: '10 ключевых архитектурных трендов: что будет популярно в ближайшие годы?',
+        category: "Технологии"
+    },
+    {
         url: Three,
         date: '03.11.2024',
         title: '10 ключевых архитектурных трендов: что будет популярно в ближайшие годы?',
         category: "Мировые тренды"
     },
+    {
+        url: Two,
+        date: '03.11.2024',
+        title: '10 ключевых архитектурных трендов: что будет популярно в ближайшие годы?',
+        category: "Ассоциация"
+    },
+    {
+        url: One,
+        date: '03.11.2024',
+        title: '10 ключевых архитектурных трендов: что будет популярно в ближайшие годы?',
+        category: "Урбанистика"
+    },
 ]
 
+const Cotegory = [
+    { cotegory: "Все статьи" },
+    { cotegory: "Архитектура" },
+    { cotegory: "Дизайн интерьеров" },
+    { cotegory: "Мировые тренды" },
+    { cotegory: "Технологии" },
+    { cotegory: "Урбанистика" },
+    { cotegory: "Ассоциация" },
+]
 
 
 const Blog: FC = () => {
     const [mobileActiveFilter, setMobileActiveFilter] = useState(false)
     const [activeFilter, setActiveFilter] = useState('Все статьи')
+    const blogContainerRef = useRef<HTMLDivElement | null>(null)
     const filterRef = useRef<HTMLDivElement | null>(null) // Ref для анимации элементов
+
 
     const handleActiveFilter = () => setMobileActiveFilter(!mobileActiveFilter)
 
@@ -70,6 +100,17 @@ const Blog: FC = () => {
         }
     }, [mobileActiveFilter])
 
+    useEffect(() => {
+        if (blogContainerRef.current) {
+            gsap.fromTo(
+                blogContainerRef.current.children,
+                { opacity: 0, y: 20 },
+                { opacity: 1, y: 0, duration: 0.4, stagger: 0.2, ease: "power.out" }
+            )
+        }
+    }, [activeFilter])
+
+
 
 
     const filteredBlogData = activeFilter === 'Все статьи'
@@ -77,12 +118,40 @@ const Blog: FC = () => {
         : BlogData.filter(item => item.category === activeFilter)
 
 
+        // Tanlangandan Keyin OChirsh
+        const handleMobileFilterSelect = (cotegory: string) => {
+            setActiveFilter(cotegory)
+            setMobileActiveFilter(false)
+        }
+
+
+
+
     return (
         <div className='mt-[80px] 2xl:mt-[200px]  px-[20px] 4xl:px-[240px] 2xl:px-[50px]'>
             <p className="text-[26px]  uppercase font-jost leading-[32px] 2xl:text-[45px] 2xl:leading-[59px]  ">
                 блог
             </p>
-            <button onClick={handleActiveFilter} className='w-full mt-[20px] flex flex-row justify-between pb-[13px] border-b border-b-[#222E51]'>
+
+            <div className='hidden 2xl:flex flex-row  mt-[40px]'>
+                {Cotegory.map((item, index) => (
+                    <button
+                        onClick={() => setActiveFilter(item.cotegory)}
+                        key={index}
+                        className={`${index !== 0 ? 'px-[30px] pt-0 pb-[12px]' : 'pb-[12px]'
+                            } border-b ${activeFilter === item.cotegory
+                                ? 'border-b-2 border-[#222E51] text-[#222E51]'
+                                : 'border-b border-[#E4E4E4] text-[#000000]'
+                            }`}
+                    >
+                        <p className='text-[22px] font-medium'>{item.cotegory}</p>
+                    </button>
+                ))}
+
+            </div>
+
+            {/* MOBILE ACTIVE FILTERS */}
+            <button onClick={handleActiveFilter} className='w-full mt-[20px] 2xl:hidden flex flex-row justify-between pb-[13px] border-b border-b-[#222E51]'>
                 <p className='text-[15px] font-medium font-jost text-[#222E51]'>
                     {activeFilter}
                 </p>
@@ -92,28 +161,32 @@ const Blog: FC = () => {
             </button>
             {mobileActiveFilter && (
                 <div ref={filterRef}>
-                    <p onClick={() => setActiveFilter('Архитектура')} className='text-[15px] font-medium font-jost text-[#222E51] w-full mt-[20px] flex flex-row justify-between pb-[13px] border-b border-b-[#222E51]'>Архитектура <MdArchitecture /></p>
-                    <p onClick={() => setActiveFilter('Дизайн интерьеров ')} className='text-[15px] font-medium font-jost text-[#222E51] w-full mt-[20px] flex flex-row justify-between pb-[13px] border-b border-b-[#222E51]'>Дизайн интерьеров  <SiAffinitydesigner /></p>
-                    <p onClick={() => setActiveFilter('Мировые тренды')} className='text-[15px] font-medium font-jost text-[#222E51] w-full mt-[20px] flex flex-row justify-between pb-[13px] border-b border-b-[#222E51]'>Мировые тренды <AiOutlineGlobal /></p>
-                    <p onClick={() => setActiveFilter('Технологии')} className='text-[15px] font-medium font-jost text-[#222E51] w-full mt-[20px] flex flex-row justify-between pb-[13px] border-b border-b-[#222E51]'>Технологии
+                    {/* <p onClick={() => handleMobileFilterSelect('Все статьи')} className='text-[15px] font-s font-jost text-[#222E51] w-full mt-[20px] flex flex-row justify-between pb-[13px] border-b border-b-[#222E51]'>Все статьи <FaBorderAll /></p> */}
+                    <p onClick={() => handleMobileFilterSelect('Архитектура')} className='text-[15px] font-semibold font-jost text-[#222E51] w-full mt-[20px] flex flex-row justify-between pb-[13px] border-b border-b-[#222E51]'>Архитектура <MdArchitecture /></p>
+                    <p onClick={() => handleMobileFilterSelect('Дизайн интерьеров ')} className='text-[15px] font-semibold font-jost text-[#222E51] w-full mt-[20px] flex flex-row justify-between pb-[13px] border-b border-b-[#222E51]'>Дизайн интерьеров  <SiAffinitydesigner /></p>
+                    <p onClick={() => handleMobileFilterSelect('Мировые тренды')} className='text-[15px] font-semibold font-jost text-[#222E51] w-full mt-[20px] flex flex-row justify-between pb-[13px] border-b border-b-[#222E51]'>Мировые тренды <AiOutlineGlobal /></p>
+                    <p onClick={() => handleMobileFilterSelect('Технологии')} className='text-[15px] font-medium font-jost text-[#222E51] w-full mt-[20px] flex flex-row justify-between pb-[13px] border-b border-b-[#222E51]'>Технологии
                         <GrTechnology />
                     </p>
-                    <p onClick={() => setActiveFilter('Урбанистика')} className='text-[15px] font-medium font-jost text-[#222E51] w-full mt-[20px] flex flex-row justify-between pb-[13px] border-b border-b-[#222E51]'>Урбанистика
+                    <p onClick={() => handleMobileFilterSelect('Урбанистика')} className='text-[15px] font-semibold font-jost text-[#222E51] w-full mt-[20px] flex flex-row justify-between pb-[13px] border-b border-b-[#222E51]'>Урбанистика
                         <GiTurban />
                     </p>
-                    <p onClick={() => setActiveFilter('Ассоциация')} className='text-[15px] font-medium font-jost text-[#222E51] w-full mt-[20px] flex flex-row justify-between pb-[13px] border-b border-b-[#222E51]'>Ассоциация
+                    <p onClick={() => handleMobileFilterSelect('Ассоциация')} className='text-[15px] font-semibold font-jost text-[#222E51] w-full mt-[20px] flex flex-row justify-between pb-[13px] border-b border-b-[#222E51]'>Ассоциация
                         <FaDrawPolygon />
                     </p>
                 </div>
             )}
 
-            <div className='mt-[20px] 2xl:mt-[41px] flex flex-col gap-[30px] 2xl:flex-row 2xl:gap-[20px]'>
-                {filteredBlogData.map((item, index) => (
-                    <div key={index}>
-                        <Image src={item.url} width={345} height={345} quality={100} alt='blogIMage' className='object-cover h-[220px] 2xl:w-full md:h-[280px]' />
+            <div ref={blogContainerRef} className='mt-[20px] 2xl:mt-[41px] flex flex-col gap-[30px] 2xl:flex-row 2xl:gap-[20px]'>
+                {filteredBlogData.slice(0, 4).map((item, index) => (
+                    <div  key={index} className='2xl:w-[349px]'>
+                        <Image src={item.url} width={345} height={345} quality={100} alt='blogIMage' className=' w-full object-cover h-[220px] 2xl:w-full md:h-[280px]' />
                         <div className='mt-[20px] 2xl:mt-[25px]'>
                             <p className='text-[14px] text-[#A0A0A0] 2xl:text-[17px]'>{item.date}</p>
-                            <p className='text-[20px] mt-[5px] font-medium font-jost text-titleDark 2xl:text-[25px] leading-[25px] 2xl:leading-[30px]'>{item.title}</p>
+                            <div className='2xl:h-[120px]'>
+                                <p className='text-[20px] mt-[5px] font-medium font-jost text-titleDark 2xl:text-[25px] leading-[25px] 2xl:leading-[30px]'>{item.title.length > 72 ? item.title.slice(0, 72) + "..." : item.title}</p>
+                            </div>
+
                             <p className='text-[16px] mt-[8px] font-medium text-[#222E51] font-jost 2xl:text-[20px] flex flex-row items-center'>
                                 Читать статью
                                 <GrLinkNext className='ml-[8px]' />
