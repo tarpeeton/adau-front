20cbed95b422112dad528825e3cab37a82ecc0a1
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-11-08 12:25:42 +0500

    @feat-socail

--- components/Contacts/Social.tsx
+++ components/Contacts/Social.tsx
@@ -1,16 +1,57 @@
 import { FC } from 'react'
 import Image from 'next/image'
 
+
+
+import Instagram from '@/public/socials/inst.png'
+import Telegram from '@/public/socials/telegram.png'
+import Facebook from '@/public/socials/facebook.png'
+import LinkedIn from '@/public/socials/linkedin.png'
+import Link from 'next/link'
+
+
+
+const DataSocialsLinks = [
+    { id: 1, title: 'Instagram', name: 'adau_uz', link: 'https://www.instagram.com/adau_uz', img: Instagram },
+    { id: 2, title: 'Facebook', name: 'adau_uz', link: 'https://www.facebook.com/adau_uz', img: Facebook },
+    { id: 3, title: 'LinkedIn', name: 'adau_uz', link: 'https://www.linkedin.com/in/adau_uz', img: LinkedIn },
+    { id: 4, title: 'Telegram', name: 'adau_uz', link: 'https://t.me/adau_uz', img: Telegram },
+]
+
+
+
+
+
+
+
+
+
 const Socials: FC = () => {
     return (
         <div className='px-[16px] 2xl:px-[50px] 4xl:px-[240px] mt-[80px] 2xl:mt-[200px]'>
             <p className='text-[24px] 2xl:text-[45px] text-titleDark font-jost uppercase'>
                 Мы в социальных сетях
             </p>
-            <div className='flex 2xl:flex-row 2xl:justify-between'>
-                <div className='2xl:w-[23%] bg-[#F7F8FA] py-[15px] px-[10px]'>
+            <div className='flex flex-col gap-[30px] 2xl:flex-row justify-between mt-[30px]'>
+                {DataSocialsLinks.map((item) => (
+                    <div key={item.id} className=' h-[200px] 2xl:w-[23%] bg-[#F7F8FA] py-[15px] px-[10px] 2xl:p-[30px] flex flex-col justify-between 2xl:h-[300px]'>
+                        <div className='flex flex-row justify-between'>
+                            <div>
+                                <p className='text-titleDark text-[24px] 2xl:text-[30px] font-jost'>{item.title}</p>
+                                <p className='text-[#222E51] text-[18px] 2xl:text-[20px] font-jost'>{item.name}</p>
+                            </div>
+                            <Image src={item?.img} quality={100} width={71} height={71} alt='instagram' className='object-cover 2xl:w-[90px]' />
 
-                </div>
+                        </div>
+                        <Link href={item.link} className='buttonBlue'>
+                            Подписаться
+                        </Link>
+                    </div>
+                ))}
+
+
+
+
             </div>
         </div>
     )
