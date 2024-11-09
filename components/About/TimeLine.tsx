1501f0dd0b0f956a7dfcaec446d47efee2246cdd
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-11-09 11:57:07 +0500

    @desktop-timeLINE

--- components/About/TimeLine.tsx
+++ components/About/TimeLine.tsx
@@ -18,11 +18,13 @@ const Timeline: React.FC = () => {
         if (containerRef.current && allCards.length > 0) {
             // Initial setup of all cards and borders
             allCards.forEach((card) => {
-                gsap.set(card, { backgroundColor: '#222E51', color: '#FFFFFF' })
+                gsap.set(card, { backgroundColor: '#222E51', color: '#fff' })
                 const innerDiv = card.querySelector(`.inner-border`)
                 if (innerDiv) {
                     gsap.set(innerDiv, { borderColor: '#FFFFFF' })
                 }
+                const textElements = card.querySelectorAll('p');
+                textElements.forEach((text) => gsap.set(text, { color: '#FFFFFF' , opacity: '50%' }));
             })
 
             // Calculate the scroll width needed for the animation
@@ -59,37 +61,49 @@ const Timeline: React.FC = () => {
                         const activeId = topCardsRef.current[closestCardIndex]?.dataset.id
 
                         allCards.forEach((card) => {
-                            const innerDiv = card.querySelector(`.inner-border`)
+                            const innerDiv = card.querySelector(`.inner-border`);
+                            const dateText = card.querySelector(`#date`);
+                            const titleText = card.querySelector(`#title`);
+                            const descriptionText = card.querySelector(`#description`);
+    
                             if (card.dataset.id === activeId) {
+                                // Active card styles
                                 gsap.to(card, {
                                     backgroundColor: '#FFFFFF',
                                     color: '#3A476D',
-                                    duration: 0.5,
-                                })
-                                if (innerDiv) {
-                                    gsap.to(innerDiv, {
-                                        borderColor: '#3A476D',
-                                        duration: 0.5,
-                                    })
-                                }
-                            } else {
-                                gsap.to(card, {
-                                    backgroundColor: '#222E51',
-                                    color: '#FFFFFF',
-                                    duration: 0.5,
-                                })
+                                    duration: 0.2,
+                                });
                                 if (innerDiv) {
                                     gsap.to(innerDiv, {
                                         borderColor: '#FFFFFF',
-                                        duration: 0.5,
-                                    })
+                                        duration: 0.2,
+                                    });
                                 }
+                                if (dateText) gsap.to(dateText, { color: '#222E51', opacity: 1, duration: 0.2 });
+                                if (titleText) gsap.to(titleText, { color: '#121212', opacity: 1, duration: 0.2 });
+                                if (descriptionText) gsap.to(descriptionText, { color: '#414141', opacity: 1, duration: 0.2 });
+                            } else {
+                                // Non-active card styles
+                                gsap.to(card, {
+                                    backgroundColor: '#3A476D',
+                                    color: '#FFFFFF',
+                                    duration: 0.2,
+                                });
+                                if (innerDiv) {
+                                    gsap.to(innerDiv, {
+                                        borderColor: '#3A476D',
+                                        duration: 0.2,
+                                    });
+                                }
+                                if (dateText) gsap.to(dateText, { color: '#FFFFFF', opacity: 0.5, duration: 0.2 });
+                                if (titleText) gsap.to(titleText, { color: '#FFFFFF', opacity: 0.5, duration: 0.2 });
+                                if (descriptionText) gsap.to(descriptionText, { color: '#FFFFFF', opacity: 0.5, duration: 0.2 });
                             }
-                        })
+                        });
 
                         // Update the progress bar width based on the active card
                         const progressWidth = ((closestCardIndex) / topCardsRef.current.length) * 45
-                        gsap.to(progressBarRef.current, { width: `${progressWidth}%`, duration: 0.4 })
+                        gsap.to(progressBarRef.current, { width: `${progressWidth}%`, duration: 0.3 })
                     },
                 },
             })
@@ -103,13 +117,12 @@ const Timeline: React.FC = () => {
    
 
     return (
-        <div ref={containerRef} className="overflow-hidden bg-[#222E51] py-[40px] px-[16px] 2xl:py-[100px] 2xl:pl-[50px] 4xl:px-[200px]">
-            <p className="text-[26px] leading-[32px] 2xl:text-[45px] 2xl:leading-[59px] text-white font-jost">
+        <div  ref={containerRef} className="overflow-hidden bg-[#222E51] py-[40px] px-[16px] 2xl:py-[100px] 2xl:pl-[50px] 4xl:px-[200px]">
+            <p  className="text-[26px] leading-[32px] 2xl:text-[45px] 2xl:leading-[59px] text-white font-jost">
                 НАШ ПУТЬ И ПЛАНЫ НА БУДУЩЕЕ
             </p>
-
-            <div>
-                <div className="topCARD Sw-full flex flex-row flex-nowrap gap-[120px] mt-[40px]">
+            <div className=' overflow-hidden pl-[120]' >
+                <div className="topCARD Sw-full flex flex-row flex-nowrap gap-[120px] mt-[40px] ">
                     {[...Array(8)].map((_, index) => (
                         <div
                             key={`top-${index}`}
@@ -120,15 +133,15 @@ const Timeline: React.FC = () => {
                             className="card flex flex-col justify-between bg-[#3A476D] min-w-[400px] transition-colors duration-500 ease-in-out h-[250px]"
                         >
                             <div className="p-[25px]">
-                                <p className="text-[#222E51] text-[25px] font-jost font-medium">2020</p>
+                                <p id='date' className="text-[25px] font-jost font-medium">2020</p>
                                 <div className="h-[150px] mt-[40px]">
-                                    <p className="text-[#121212] text-[25px] font-jost font-medium">Основание ассоциации</p>
-                                    <p className="text-[#414141] text-[18px] font-jost font-medium">
+                                    <p id='title' className=" text-[25px] font-jost font-medium">Основание ассоциации</p>
+                                    <p id='description' className=" text-[18px] font-jost font-medium">
                                         Lorem ipsum dolor sit amet consectetur. Consectetur ultrices volutpat posuere pharetra tempor massa habе
                                     </p>
                                 </div>
                             </div>
-                            <div className="inner-border w-full h-[30px] mt-[-20px] border-dashed border-l-[2px] text-[#222E51] border-[#3E4A6A]">rustam</div>
+                            <div className="inner-border w-full h-[30px] mt-[-20px] border-dashed border-l-[2px] text-[#222E51] border-white">rustam</div>
                         </div>
                     ))}
                 </div>
@@ -145,14 +158,14 @@ const Timeline: React.FC = () => {
                                 if (el) bottomCardsRef.current[index] = el
                             }}
                             data-id={`card-${index}`}
-                            className="card flex flex-col justify-between bg-[#3A476D] min-w-[400px] transition-colors duration-500 ease-in-out h-[250px]"
+                            className="card flex flex-col justify-between  min-w-[400px] transition-colors duration-300 ease-in-out h-[250px]"
                         >
                             <div className="inner-border w-full h-[30px] mt-[-20px] border-dashed border-r-[2px] border-[#3E4A6A] text-[#222E51]">rustam</div>
                             <div className="p-[25px]">
-                                <p className="text-[#222E51] text-[25px] font-jost font-medium">2021</p>
+                                <p id='date' className="text-[#222E51] text-[25px] font-jost font-medium">2021</p>
                                 <div className="h-[150px] mt-[40px]">
-                                    <p className="text-[#121212] text-[25px] font-jost font-medium">Проведение семинара</p>
-                                    <p className="text-[#414141] text-[18px] font-jost font-medium">
+                                    <p id='title' className="text-[#121212] text-[25px] font-jost font-medium">Проведение семинара</p>
+                                    <p id='description' className="text-[#414141] text-[18px] font-jost font-medium">
                                         Lorem ipsum dolor sit amet consectetur. Consectetur ultrices volutpat posuere pharetra tempor massa habе
                                     </p>
                                 </div>
