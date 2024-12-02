45e572efa1f713745420ce4d7b635ad085f98fa8
tarpeeton <146853258+tarpeeton@users.noreply.github.com>
2024-12-02 11:09:12 +0500

    @logo

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,8 +5,7 @@ import type { Metadata } from 'next';
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import ClientQueryProvider from '@/providers/useReactQuery';
-
-
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'ADAU | HOME',
@@ -28,11 +27,38 @@ export default async function LocaleLayout({
   // `locale`ni `getMessages`ga uzatish
   const messages = await getMessages();
 
-  // React Query uchun yangi `QueryClient` yaratish
-  
-
   return (
     <html lang={locale}>
+      <head>
+        <meta name="yandex-verification" content="62db76289f18bce8" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        {/* Inject Yandex Metrika script with next/script */}
+        <Script 
+          strategy="afterInteractive" 
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; 
+                m[i].l=1*new Date(); 
+                for (var j = 0; j < document.scripts.length; j++) {
+                  if (document.scripts[j].src === r) { return; }
+                } 
+                k=e.createElement(t),
+                a=e.getElementsByTagName(t)[0],
+                k.async=1,
+                k.src=r,
+                a.parentNode.insertBefore(k,a);
+              })
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); 
+              ym(99098178, "init", { 
+                clickmap:true, 
+                trackLinks:true, 
+                accurateTrackBounce:true 
+              });
+            `,
+          }}
+        />
+      </head>
       <body>
         <ClientQueryProvider>
           <NextIntlClientProvider messages={messages}>
